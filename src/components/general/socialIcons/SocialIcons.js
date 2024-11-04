d9798fff00c8802eb5d7c39591fccd425d66347a
Pablo Cuba <pablofcc97@gmail.com>
2024-11-04 13:51:16 -0500

    social href changed

--- src/components/general/socialIcons/SocialIcons.js
+++ src/components/general/socialIcons/SocialIcons.js
@@ -14,7 +14,7 @@ const SocialIcons = () => {
             <a href="https://github.com/pablofcc97" target="_blank" rel="noreferrer"  className="socialIcon__link">
                 <img src={GithubImage} alt="" className="socialIcon__img" />
             </a>
-            <a href="https://wa.me/51921736454?text=Hola%20vengo%20de%20tu%20portafolio%20" rel="noreferrer" target="_blank" className="socialIcon__link">
+            <a href="https://wa.me/51935536512?text=Hola%20vengo%20de%20tu%20portafolio%20" rel="noreferrer" target="_blank" className="socialIcon__link">
                 <img src={WhatsappImage} alt="" className="socialIcon__img" />
             </a>
             <a href="mailto:pablofcc97@gmail.comsubject=Portafolio Pablo Cuba&body=Hola%20vengo%20de%20tu%20portafolio%20" rel="noreferrer" target="_blank" className="socialIcon__link">
